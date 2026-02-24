901d34bc51ac1dcbc57eca4a2eb8941397644652
Saswat Kumar Dash <saswatkumardash@evawinoptimize.com>
2026-02-24 13:55:04 +0530

    Added project structure UI and functionality

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,22 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
 	base: "./",
+
 	build: {
 		outDir: "dist",
+
+		rollupOptions: {
+			input: {
+				main: "index.html",
+
+				project1: "src/project1/index.html",
+
+				project2: "src/geometries-project2/index.html",
+
+				project3: "src/Curves2D-project3/index.html",
+
+				project4: "src/polygonCutting-project4/index.html",
+			},
+		},
 	},
 });
