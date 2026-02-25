0f258311bb9dd344353943b614c33d9ffda0c597
SaswatDash-EvA <saswatkumardash@evawinoptimize.com>
2026-02-25 17:03:44 +0530

    Added V cut on extruded cuboid

---
+++ src/polygonCutting-project4/extrudeShapeCutting.ts
@@ -0,0 +1,52 @@
+import { activeGeometry, angle1, angle2, cleanCylinderGeometry, cleanTrapizoidGeometry, dynamicSquareHeight, extrudeCircleMajorRadius, extrudeDepth, extrudeRectangleWidth } from "./geometries";
+
+let cutWidth1 = 0.25, cutHeight1 = 0.25;
+let cutWidth2 = 0.25, cutHeight2 = 0.25;
+
+export function cutPolygon() {
+    const positionAttribute = activeGeometry.attributes.position;
+    const vertexArray = positionAttribute.array;
+
+    if (activeGeometry === cleanTrapizoidGeometry) {
+        for (let i = 0; i < vertexArray.length; i += 3) {
+            if (vertexArray[i+1] == -extrudeRectangleWidth/2) continue;
+
+            if (((extrudeRectangleWidth / Math.tan(angle1)) + (extrudeRectangleWidth / Math.tan(angle2))) <= extrudeDepth) {
+                if (vertexArray[i+2] > 0)
+                    vertexArray[i+2] -= 2 * vertexArray[i+1] / Math.tan(angle1);
+                else vertexArray[i+2] += 2 * vertexArray[i+1] / Math.tan(angle2);
+            }
+            else {
+                const y = Math.sin(angle1) * Math.cos(angle2) * extrudeDepth * Math.abs(2 * vertexArray[i+1] / extrudeRectangleWidth) / Math.sin(angle1 + angle2);
+                vertexArray[i+2] = -extrudeDepth/2 + y;
+                vertexArray[i+1] = -extrudeRectangleWidth/2 + y * Math.tan(angle2)
+            }
+            
+        }
+    }
+    else if (activeGeometry === cleanCylinderGeometry) {
+        for (let i = 0; i < vertexArray.length; i += 3) {
+            if (vertexArray[i+1] == -extrudeCircleMajorRadius) continue;
+
+            if (((2 * extrudeCircleMajorRadius / Math.tan(angle1)) + (2 * extrudeCircleMajorRadius / Math.tan(angle2))) <= extrudeDepth) {
+                if (vertexArray[i+2] > 0)
+                    vertexArray[i+2] -= (vertexArray[i+1] + extrudeCircleMajorRadius) / Math.tan(angle1);
+                else vertexArray[i+2] += (vertexArray[i+1] + extrudeCircleMajorRadius) / Math.tan(angle2);
+            }            
+        }
+    }
+    else if ((cutWidth1 + cutWidth2) < extrudeDepth) {
+        for (let i = 0; i < vertexArray.length; i += 3) {
+            if (vertexArray[i+1] == 0) {
+                if (vertexArray[i+2] > 0) vertexArray[i+1] += -dynamicSquareHeight/2 + cutHeight1;
+                else vertexArray[i+1] += -dynamicSquareHeight/2 + cutHeight2;
+                continue;
+            }
+
+            if (vertexArray[i+2] > 0)
+                vertexArray[i+2] -= cutWidth1;
+            else vertexArray[i+2] += cutWidth2;
+        }
+    }
+    positionAttribute.needsUpdate = true;
+}
--- src/polygonCutting-project4/geometries.ts
+++ src/polygonCutting-project4/geometries.ts
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import { BufferGeometryUtils } from 'three/examples/jsm/Addons.js';
-import { cutPolygon } from './boxCutting';
+import { cutPolygon } from './extrudeShapeCutting';
 
 export let extrudeRectangleLength = 0.5, extrudeRectangleWidth = 0.5, extrudeDepth = 2;
 export let angle1 = Math.PI/4, angle2 = Math.PI/4;
@@ -38,10 +38,41 @@ cylinderExtrudeGeometry.deleteAttribute("normal");
 cylinderExtrudeGeometry.deleteAttribute("uv");
 export const cleanCylinderGeometry = BufferGeometryUtils.mergeVertices(cylinderExtrudeGeometry);
 
-export let activeGeometry = cleanCylinderGeometry;
+// Dynamic Rectangle cut with V shape
+export let dynamicSquareWidth = 0.5, dynamicSquareHeight = 0.5;
+const dynamicShape = new THREE.Shape();
+dynamicShape.moveTo(dynamicSquareWidth/2, dynamicSquareWidth/2);
+dynamicShape.lineTo(dynamicSquareWidth/2, 0);
+dynamicShape.lineTo(dynamicSquareWidth/2, -dynamicSquareWidth/2);
+dynamicShape.lineTo(-dynamicSquareWidth/2, -dynamicSquareHeight/2);
+dynamicShape.lineTo(-dynamicSquareHeight/2, 0);
+dynamicShape.lineTo(-dynamicSquareWidth/2, dynamicSquareHeight/2);
+
+const dynamicRectangleGeometry = new THREE.ExtrudeGeometry(dynamicShape, {
+    depth: extrudeDepth,
+    bevelEnabled: false,
+});
+dynamicRectangleGeometry.translate(0, 0, -extrudeDepth/2);
+
+dynamicRectangleGeometry.deleteAttribute("normal");
+dynamicRectangleGeometry.deleteAttribute("uv");
+export const cleanDynamicGeometry = BufferGeometryUtils.mergeVertices(dynamicRectangleGeometry);
+const indexArray = cleanDynamicGeometry.index;
+if (indexArray) {
+    indexArray.array[20] = 9;
+    indexArray.array[23] = 6;
+    indexArray.array[8] = 5;
+    indexArray.array[11] = 2;
+    indexArray.needsUpdate = true;
+}
+
+console.log(cleanDynamicGeometry.attributes.position.array);
+console.log(cleanDynamicGeometry.index?.array)
+export let activeGeometry = cleanDynamicGeometry;
 
 cutPolygon();
 
-export function updateActiveGeometry() {
-    activeGeometry = activeGeometry === cleanCylinderGeometry? cleanTrapizoidGeometry : cleanCylinderGeometry;
+export function updateActiveGeometry(index: number) {
+    const geometries: Array<THREE.BufferGeometry> = [cleanTrapizoidGeometry, cleanCylinderGeometry, cleanDynamicGeometry];
+    activeGeometry = geometries[index];
 }
